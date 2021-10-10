7bf9e730587f242f374c7135c1d6949fccf23acc
xiaobai-web715 <liuxinghua715@163.com>
2021-10-10 11:24:38 +0800

    项目准备

---
+++ blog-frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RouterFn from './router'
+
+const App = () => {
+    return (
+        <div>
+            <RouterFn/>
+        </div>
+    )
+}
+
+export default App
---
+++ blog-frontend/src/router.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RouterFn = () => {
+    return (
+        <div>
+           路由 
+        </div>
+    )
+}
+
+export default RouterFn
